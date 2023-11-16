39c83be1e169824bb946782688ba525a49695087
Evan junior <evsioljr@gmail.com>
2023-11-16 11:34:44 -0300

    adm flow

--- src/pages/adm/adm.jsx
+++ src/pages/adm/adm.jsx
@@ -1,26 +1,86 @@
 import React, { useState } from 'react';
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, InputNumber, Flex } from "antd";
 import { useNavigate } from 'react-router-dom';
 import './adm.css';
+import Cardapio from '../home/cardapio';
+
 
 
 function Adm() {
- 
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
-   <div>
-        
+    <div>
 
-        <div id="bttn-op" className="tabs">
-                <button className="butto"  >COMIDAS</button>
-                <button className="butto"  >BEBIDAS</button>
-        </div>
-        
-        <div id='container-btn-adm'>
 
-          
+      <nav> RESTAURANTE</nav>
+
+      <div id='containeradm' style={{ height: "70vh", width: "100vw", paddingTop: "5vh" }}>
+        <div id='containeradm-2' style={{ height: "63vh", width: "80vw", margin: "auto", borderRadius: "0.5rem", padding: "1rem" }}>
+          <div id='container-3'>
+            <div className='divs'>
+              <p>NOME</p>
+              <input type="text" style={{width:"100vh"}} />
+
+            </div>
+            <div className='divs'>
+              <p>VALOR</p>
+              <input type="number" style={{width:"7vh"}}/>
+
+            </div>
+            <div className='divs'>
+              <p>IMAGEM</p>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+
+            </div>
+            <div className='divs'>
+              <p>tempo</p>
+              <input type="time" />
+
+            </div>
+            <div className='divs'>
+              <p>PESSOAS</p>
+              <input type="text" style={{width:"10vh" , height:"3vh"}} />
+
+            </div>
+          </div>
+          <div>
+            <p style={{margin:"15px"}}>PDESCRIÇÃO</p>
+            <input type="text" style={{height:"12vh" , width:"80vw" , borderRadius:"8px"}} />
+
+          </div>
+          <button className="butto" style={{borderRadius:"10px", width:"80vw", marginTop:"1.5vh"}}>ENVIAR</button>
+
+
+
+
+
         </div>
-   </div>
+      </div>
+      <div id='container-btn-adm' style={{ marginTop: "3vh" }}>
+
+        <Cardapio></Cardapio>
+
+      </div>
+    </div>
 
   )
 }
